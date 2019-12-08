2d42f74619a5c270bb9691d70517dee82622d2cd
Luis Enrique Cabrera <adriana@home.com>
2019-12-08 14:56:27 +0100

    n

--- 07_microservicio_log_policia/src/main/java/daos/DaoPolicia.java
+++
@@ -1,9 +0,0 @@
-package daos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.LogPolicia;
-
-public interface DaoPolicia extends JpaRepository<LogPolicia, Integer> {
-	
-}
--- 07_microservicio_log_policia/src/main/java/service/ServicePolicia.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import model.LogPolicia;
-
-public interface ServicePolicia {
-	void guardarRegistroAlarma(LogPolicia log);
-
-}
